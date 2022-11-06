7670b2cc8067bfb95abb9065b9fc6393b27eec86
Yogya2502 <114391409+Yogya2502@users.noreply.github.com>
2022-11-06 23:12:55 +0530

    Code to create Linked List

---
+++ linked_list.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+ // Node is a user defined data type therefore we use struct 
+struct Node {
+    int data;
+    struct Node* next;
+};
+  
+int main()
+{
+    /* we will take four pointers of the same datatype and value stored in each one of them is NULL*/
+    struct Node* head = NULL;
+    struct Node* second = NULL;
+    struct Node* third = NULL;
+    struct Node* fourth = NULL;
+  
+    // allocate 4 nodes in the heap ( dynamic memory allocation)
+    head = (struct Node*)malloc(sizeof(struct Node));
+    second = (struct Node*)malloc(sizeof(struct Node));
+    third = (struct Node*)malloc(sizeof(struct Node));
+    fourth = (struct Node*)malloc(sizeof(struct Node));
+
+    head->data = 5; 
+    head->next = second;
+
+    second->data = 10;
+    second->next = third;
+  
+    third->data = 15; 
+    third->next = fourth;
+  
+    fourth->data = 20;
+    fourth->next = NULL;
+
+    printlist(head);
+
+    return 0;
+}
+
+int printlist(struct Node* n)
+{
+    while (n != NULL) {
+        printf(" %d ", n->data);
+        n = n->next;
+    }
+}
