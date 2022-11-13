0530e8d3be4a413edc223de715d677bd5439f18b
Yogya2502 <114391409+Yogya2502@users.noreply.github.com>
2022-11-13 19:43:28 +0530

    Update Chunk_Reversal.c

--- Chunk_Reversal.c
+++ Chunk_Reversal.c
@@ -5,35 +5,32 @@ struct Node
 { int data;
     struct Node* next;
 };
- // we divide the given linked list into sub lists of size n and link the pointer to the new head node
-struct Node *Rev(struct Node *head, int n) 
+ 
+struct Node *reverse (struct Node *head, int k)
 {
     if (!head)
         return NULL;
    
-    struct Node* curr = head;
+    struct Node* current = head;
     struct Node* next = NULL;
     struct Node* prev = NULL;
-    int flag = 0;
- // we start by reversing the first sub list of group n.
+    int count = 0;
    
-    while (curr != NULL && flag < n)
+    while (current != NULL && count < k)
     {
-        next  = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = next;
-        flag++;
+        next  = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+        count++;
     }
- //now the last node of the first sub list after reversal is a pointer to the (n+1)th node.
- // the entire list is reversed recursively starting from curr ans sub lists.
     if (next !=  NULL)
-       head->next = Rev(next, n);
+       head->next = reverse(next, k);
  
-    return prev; // the list provided by the user will have prev as the head of the list.
+    return prev;
 }
  
-void push(struct Node** ref, int new_data) // we have created this function in order to push a node 
+void push(struct Node** ref, int new_data)
 {
 
     struct Node* new_node =
@@ -43,12 +40,9 @@ void push(struct Node** ref, int new_data) // we have created this function in o
  
     new_node->next = (*ref);   
     (*ref)    = new_node;
- /* in this function we allocated a node in the heap memory along with inserting the data in the node. 
- the last node of the old list has been now linked to this node and the head pointer has been moved to 
- the new node */
 }
  
-void printlist(struct Node *node) // we have created this function to print the prepared linked list.
+void printlist(struct Node *node)
 {
     while (node != NULL)
     {
@@ -68,12 +62,11 @@ int main(void)
      push(&head, 50);
      push(&head, 40);
      push(&head, 30);
-     push(&head, 20);        
- // now calling the push function with the help of which we will create the linked list. 
+     push(&head, 20);          
  
      printf("\n Inputted linked list  \n");
      printlist(head);
-     head = Rev(head, 3);
+     head = reverse(head, 3);
  
      printf("\n Reversed Linked list \n");
      printlist(head);
